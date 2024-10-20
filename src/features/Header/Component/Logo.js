5d90c6a244fccdad7e39b76f2efd886ab2455e71
adi_dev_97 <93173682+AdiDEV97@users.noreply.github.com>
2024-10-20 18:46:51 +0530

    Update Logo.js

--- src/features/Header/Component/Logo.js
+++ src/features/Header/Component/Logo.js
@@ -3,6 +3,8 @@ import React from 'react'
 import Lexicon from '../../../Lexicon.png'
 import { Link } from 'react-router-dom'
 
+// Logo Component
+
 const Logo = () => {
   return (
       <Link to='/home'><div className='imgLogo'><img className='logo' src={Lexicon} alt='lexocon.png' width="90" height="80" /></div></Link>
